6998f8571f1a9e4d64892bf077cc1794442198ca
Elvis Muratigit config --global user.email elvismurati15@gmail.com <98538952+v1ct0R7@users.noreply.github.com>
2026-09-14 16:47:21 +0200

    Correct task 1

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,2 +1,5 @@
 -- All privileges and password to user_0d_1_pwd
-ALTER USER 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost'
+IDENTIFIED BY 'user_0d_1_pwd';
+
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
