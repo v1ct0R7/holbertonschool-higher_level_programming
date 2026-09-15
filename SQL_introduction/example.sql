636ab9fcf7f90c0895823fb044a17936c4686888
Elvis Muratigit config --global user.email elvismurati15@gmail.com <98538952+v1ct0R7@users.noreply.github.com>
2026-09-15 12:05:45 +0200

    Complete task 3

---
+++ SQL_introduction/example.sql
@@ -0,0 +1,10 @@
+-- data query language
+-- name_of_database -> esht emri i databazes qe po perdorim per shembujt posht
+--emri_tabeles -> esht emri i tables qe po perdorim per shembujt posht
+
+
+show DATABASES; -- tregon te gjitha databazat
+use name_of_database; -- perdor databazen qe deshiron
+show tables; -- shfaq tabelat e databazes qe po perdor
+select * from emri_tabeles; -- shfaq cdo kolon me info 
+select coloum_name from emri_tabeles; -- shfaq vetem kolonat qe deshiron
