f59e9223713f99a41274bee3a12883731b444668
Elvis Muratigit config --global user.email elvismurati15@gmail.com <98538952+v1ct0R7@users.noreply.github.com>
2026-09-15 13:52:42 +0200

    Complete task 8

---
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -0,0 +1,5 @@
+-- List all cities of the table that can be found on the database hbtn_0d_usa
+SELECT cities.id, cities.name
+FROM cities
+WHERE cities.state_id = (SELECT id FROM states WHERE name = "California")
+ORDER BY cities.id ASC;
