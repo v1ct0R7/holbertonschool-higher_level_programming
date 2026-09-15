12f98367ba6a25e5fdc3167eba0135b94e9792c7
Elvis Muratigit config --global user.email elvismurati15@gmail.com <98538952+v1ct0R7@users.noreply.github.com>
2026-09-15 13:38:06 +0200

    Make some corrections

--- SQL_more_queries/6-states.sql
+++ SQL_more_queries/6-states.sql
@@ -6,4 +6,3 @@ CREATE TABLE IF NOT EXISTS states(
     id INT UNIQUE AUTO_INCREMENT NOT NULL PRIMARY KEY,
     name VARCHAR(256) NOT NULL
 );
-INSERT INTO states (name) VALUES ("California"), ("Arizona"), ("Texas");
