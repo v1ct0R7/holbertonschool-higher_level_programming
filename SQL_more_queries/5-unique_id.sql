891e5db11d443cc44611cf7c0719a088d21849bd
Elvis Muratigit config --global user.email elvismurati15@gmail.com <98538952+v1ct0R7@users.noreply.github.com>
2026-09-15 12:43:36 +0200

    Complete task 5

--- SQL_more_queries/5-unique_id.sql
+++ SQL_more_queries/5-unique_id.sql
@@ -1,5 +1,8 @@
 -- create a table unique_id
 CREATE TABLE IF NOT EXISTS unique_id(
-    id INT NOT NULL,
+    id INT NOT NULL UNIQUE,
     name VARCHAR(256)
 );
+
+INSERT INTO unique_id(id, name)
+VALUES (89, "Best School");
