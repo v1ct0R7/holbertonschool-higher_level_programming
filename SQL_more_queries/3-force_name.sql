66dc357ffdec4dd32347852c3d048912c465b93c
Elvis Muratigit config --global user.email elvismurati15@gmail.com <98538952+v1ct0R7@users.noreply.github.com>
2026-09-15 12:37:28 +0200

    Correct task 3

--- SQL_more_queries/3-force_name.sql
+++ SQL_more_queries/3-force_name.sql
@@ -1,8 +1,10 @@
 -- Create table force_name
 CREATE TABLE IF NOT EXISTS force_name(
-    id INT NOT NULL,
-    name VARCHAR(256)
+    id INT,
+    name VARCHAR(256) NOT NULL
 );
 
 INSERT INTO force_name (id, name)
 VALUES (89, 'Best School');
+
+INSERT INTO force_name (id) VALUES (333);
