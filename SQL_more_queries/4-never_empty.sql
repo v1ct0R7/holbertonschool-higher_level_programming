2ddc2519c86c1eafb916ddf3453ff1b94c280695
Elvis Muratigit config --global user.email elvismurati15@gmail.com <98538952+v1ct0R7@users.noreply.github.com>
2026-09-15 12:33:37 +0200

    Complete task 4

---
+++ SQL_more_queries/4-never_empty.sql
@@ -0,0 +1,8 @@
+-- create a table id_not_null
+CREATE TABLE IF NOT EXISTS id_not_null(
+    id INT NOT NULL,
+    name VARCHAR(256)
+);
+
+INSERT INTO id_not_null(id, name)
+VALUES (89, "Best School"),("Best");
